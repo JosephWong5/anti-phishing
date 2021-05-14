38772120651fafc67c567dd7f6d24ba4d5d96e41
Josephwong5 <josephwong55@gmail.com>
2021-05-14 05:55:49 +0100

    removed css

--- frontend/js/plugin_ui.js
+++ frontend/js/plugin_ui.js
@@ -1,9 +1,4 @@
 var background = chrome.extension.getBackgroundPage();
-var colors = {
-  "-1": "#58bc8a",
-  "0": "#ffeb3c",
-  "1": "#ff8b66"
-};
 
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   var isPhish = background.isPhish[tabs[0].id];
